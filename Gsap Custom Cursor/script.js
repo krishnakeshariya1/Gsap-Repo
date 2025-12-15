3c4a3c29667c2e5c3d34666290044ce2af281d4a
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-12-15 18:14:39 +0530

    file added

---
+++ Gsap Custom Cursor/script.js
@@ -0,0 +1,4 @@
+const main = document.querySelector("#main");
+const cursor = document.querySelector("#cursor");
+
+main.addEventListener("")
--- Gsap svg/script.js
+++ Gsap svg/script.js
@@ -3,12 +3,6 @@ const finalPath = `M 20 100 Q 750 100 1500 100`;
 
 const string = document.querySelector(".string");
 
-// string.addEventListener("mouseenter",(dets)=>{
-//     console.log(dets);
-// });
-// string.addEventListener("mouseleave",(dets)=>{
-//     console.log(dets);
-// })
 
 string.addEventListener("mousemove",(dets)=>{
     initialPath =`M 20 100 Q ${dets.x} ${dets.y} 1500 100`;
