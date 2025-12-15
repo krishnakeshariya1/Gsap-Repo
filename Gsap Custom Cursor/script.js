0fd69001cd3e344d9599e9cbb1762be1efe63edd
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-12-15 18:55:31 +0530

    custom cursor

--- Gsap Custom Cursor/script.js
+++ Gsap Custom Cursor/script.js
@@ -1,4 +1,26 @@
 const main = document.querySelector("#main");
 const cursor = document.querySelector("#cursor");
+const img = document.querySelector(".ovlay");
 
-main.addEventListener("")
+main.addEventListener("mousemove",(dets)=>{
+    gsap.to(cursor,{
+        x : dets.x,
+        y: dets.y,
+        duration: 1
+    });
+});
+
+img.addEventListener("mouseenter",()=>{
+    cursor.textContent = "view img";
+    gsap.to(cursor,{
+        scale:2,
+        backgroundColor :"#f1f1f17e",
+    })
+});
+img.addEventListener("mouseleave",()=>{
+    cursor.textContent ="";
+    gsap.to(cursor,{
+        scale : 1,
+        backgroundColor : "#fff",
+    })
+})
