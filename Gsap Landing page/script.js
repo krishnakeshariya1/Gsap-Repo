48c158a0573adfae02a4e518291b2f182de24266
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-12-20 19:49:36 +0530

    done

--- Gsap Landing page/script.js
+++ Gsap Landing page/script.js
@@ -1,32 +1,56 @@
-const tl = gsap.timeline();
+function firstPageAnimation() {
+    const tl = gsap.timeline();
 
-tl.from("nav h1", {
-    y: -30,
-    opacity: 0,
-    delay : 0.5,
-    duration: 1,
+    tl.from("nav h1", {
+        y: -30,
+        opacity: 0,
+        delay: 0.5,
+        duration: 1,
+    })
+    tl.from("nav ul li, nav button", {
+        y: -30,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.1,
+    })
+    tl.from(".about-section .text-area h1", {
+        x: -300,
+        duration: 0.5,
+        opacity: 0,
+    })
+    tl.from(".about-section .text-area p", {
+        x: -300,
+        opacity: 0,
+        duration: 0.5,
+    })
+    tl.from(".about-section .text-area button", {
+        opacity: 0,
+        duration: 0.3,
+    })
+    tl.from(".about-section figure img", {
+        opacity: 0,
+        duration: 0.4,
+    }, "-=1")
+    tl.from(".buttom-row img", {
+        y: 50,
+        duration: 0.5,
+        opacity: 0,
+        stagger: 0.2,
+    })
+}
+firstPageAnimation();
+
+const tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".service-section",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 0",
+        scrub: 0.5,
+    }
 })
-tl.from("nav ul li, nav button", {
-    y: -30,
+tl2.from(".service-text", {
+    y: 20,
     opacity: 0,
-    duration: 1,
-    stagger : 0.1,
-})
-tl.from(".about-section .text-area h1",{
-    x: -300,
-    duration:0.5,
-    opacity: 0,
-})
-tl.from(".about-section .text-area p",{
-    x: -300,
-    opacity: 0,
-    duration: 0.5,
-})
-tl.from(".about-section .text-area button",{
-    opacity :0,
     duration: 0.3,
-})
-tl.from(".about-section figure img",{
-    opacity : 0,
-    duration:0.4,
-},"-=1")
+});
