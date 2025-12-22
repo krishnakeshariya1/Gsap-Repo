fbead7df2e1268509628f0e3b7f59543e5224cf0
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-12-22 18:06:37 +0530

    fly img animation

--- gsap2/src/App.jsx
+++ gsap2/src/App.jsx
@@ -37,23 +37,57 @@ import { useState } from "react"
 //   )
 // }
 // export default App
+// const App = () => {
+//   const [circle, setCircle] = useState(0);
+//   const random = gsap.utils.random(-700, 600, 10);
+
+//   useGSAP(() => {
+//     gsap.to(".circle", {
+//       x: circle,
+//       duration: 0.5,
+//     })
+//   },[circle])
+
+//   return (
+//     <main>
+//       <button onClick={() => {
+//         setCircle(random)
+//       }}>Animate</button>
+//       <div className="circle"></div>
+//     </main>
+//   )
+// }
+// export default App
+
 const App = () => {
-  const [circle, setCircle] = useState(0);
-  const random = gsap.utils.random(-700, 600, 10);
+
+  const Xrandom = gsap.utils.random(-700, 700, 10);
+  const Yrandom = gsap.utils.random(-500, 500, 10);
+  const rotateRandom = gsap.utils.random(-360, 360, 30);
+
+  const [xVal, setXVal] = useState(0);
+  const [yVal, setYVal] = useState(0);
+  const [rotateVal, setRotate] = useState(0);
+
+  const imgRef = useRef();
 
   useGSAP(() => {
-    gsap.to(".circle", {
-      x: circle,
-      duration: 0.5,
+    gsap.to(imgRef.current, {
+      x: xVal,
+      y: yVal,
+      rotate: rotateVal,
+      duration: 0.7,
+      delay: 0.1,
     })
-  },[circle])
-
+  },[xVal, yVal, rotateVal])
   return (
     <main>
       <button onClick={() => {
-        setCircle(random)
-      }}>Animate</button>
-      <div className="circle"></div>
+        setXVal(Xrandom);
+        setYVal(Yrandom);
+        setRotate(rotateRandom);
+      }}>fly</button>
+      <img src="https://images.vexels.com/media/users/3/242241/isolated/preview/409d95bf597e130c6c1b1d2ac3f5dbf5-side-fly-geometric-color-stroke.png" alt="fly img" ref={imgRef} />
     </main>
   )
 }
